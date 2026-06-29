626466d0ddb82881d6245c164b213a71f2aa6658
David-24245 <24245dl@hvhs.school.nz>
2026-06-29 23:43:26 +0000

    Jun 29, 2026, 11:43 PM

--- script.js
+++ script.js
@@ -29,13 +29,11 @@ function getFormInput(){
     let userName = NAME_FIELD.value;
     OUTPUT.innerHTML = "<p>Your name is " + userName + "</p>";
 
-    let item = document.getElementById("nameField").value;
-    
-    shoppingList.push(item);
-    console.log("Added: " + item);
+    for (let i = 0; i < shoppingList.length; i++) {
+        OUTPUT.innerHTML += shoppingList[i] + "<br>";
+    }
 
-    OUTPUT.innerHTML = "<p>You have added " + item + " to the list.</p>";
-    document.getElementById("itemField").value = "";
+    console.log("List displayed Items: " + shoppingList.length);
 }
 
     /*************** Order ***************/
