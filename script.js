659e732ce871b4c652efde1fdba1378e3b8d6eaa
David-24245 <24245dl@hvhs.school.nz>
2026-07-19 04:24:14 +0000

    Jul 19, 2026, 4:24 AM

--- script.js
+++ script.js
@@ -45,4 +45,27 @@ function addItem(){
     } else if (choice == 2){
         item = "Bagel";
         itemPrice = 7;
-    } else
+    } else if (choice == 3){
+        item = "Muffin";
+        itemPrice = 7;
+    } else if (choice == 4){
+        item = "Brownie";
+        itemPrice = 4;
+    } else if (choice == 5){
+        item = "Espresso";
+        itemPrice = 9;
+    } else if (choice == 6){
+        item = "Latte";
+        itemPrice = 9;
+    } else if (choice == 7){
+        item = "Hot Chocolate";
+        itemPrice = 9;
+    } else if (choice == 8){
+        item = "Cappuccino";
+        itemPrice = 9;
+    } else {
+        OUTPUT.innerHTML = "<p>Please enter a valid item number (1-8).</p>";
+        return;
+    }
+    
+}
