b8a236e0c48fa8eafac7fe33b35f9cb6051e2bba
David-24245 <24245dl@hvhs.school.nz>
2026-07-19 05:18:53 +0000

    Jul 19, 2026, 5:18 AM

--- script.js
+++ script.js
@@ -92,3 +92,18 @@ function calculateChange(_money, _price){
     let change = _money - _price;
     return change;
 }
+
+function checkPayment(){
+    const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+    let userMoney = document.getElementById("moneyField").value;
+
+    if (userMoney < orderTotal){
+        console.log("Sorry you can't afford the order");
+        OUTPUT.innerHTML = "<p>Sorry, you can't afford the order.</p>";
+    } else {
+        let change = calculateChange(userMoney, orderTotal);
+        console.log("You can afford the order");
+        OUTPUT.innerHTML = "<p>You can afford the order!</p>";
+        OUTPUT.innerHTML += "<p>You will get $" + change + " change.</p>";
+    }
+}
