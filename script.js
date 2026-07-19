16985a184ebbbf034eaa502210fe7daaff709145
David-24245 <24245dl@hvhs.school.nz>
2026-07-19 01:12:55 +0000

    Jul 19, 2026, 1:12 AM

--- script.js
+++ script.js
@@ -42,6 +42,6 @@ function addItem(){
     let name = document.getElementById("nameField").value;
 
     if (choice === ""){
-        OUTPUT.innerHTML = "<p>"
-    }
+        OUTPUT.innerHTML = "<p>Please enter a valid item number (1-8).</p>";
+    } else
 }
