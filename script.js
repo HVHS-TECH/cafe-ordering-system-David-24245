b3331776b8ecbf2530aca9390113f3c534b3f640
David-24245 <24245dl@hvhs.school.nz>
2026-07-21 09:45:45 +0000

    Jul 21, 2026, 9:45 AM

--- script.js
+++ script.js
@@ -80,10 +80,25 @@ function goToReceiptPage(){
 
     const RECEIPT_OUTPUT = document.getElementById("receiptOutput");
 
+    for (let i = 0; i < itemList.length; i+++) {
+        let currentItem = itemList[i]
+        let currentPrice = priceList[i]
+        let Index = Items1.indexOf(currentItem);
+
+        if(Index === -1) {
+            Items1.push(currentItem);
+            quantities.push(1);
+            unitPrices.push(currentPrice);
+        } else {
+            quantities[Index] += 1;
+        }
+    }
+
     RECEIPT_OUTPUT.innerHTML = "<p>Receipt for " + userName + ":</p>";
 
-    for (let i = 0; i < itemList.length; i++) {
-    RECEIPT_OUTPUT.innerHTML += itemList[i] + " - $" + priceList[i] + "<br>";
+    for (let i = 0; i < Items1.length; i++) {
+        let lineTotal = quantities[i] * unitPrices[i];
+        RECEIPT_OUTPUT.innerHTML += quantities[i] + "x " + Items1[i] + " - $" + lineTotal + "<br>";
     }
 
     RECEIPT_OUTPUT.innerHTML += "<p>Total: $" + orderTotal + "</p>";
