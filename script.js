6d91373a129b62f3c744dec544437ce6dc7854fc
David-24245 <24245dl@hvhs.school.nz>
2026-07-22 21:46:31 +0000

    Jul 22, 2026, 9:46 PM

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ function updateCartSummary(){
         let lineTotal = quantities[i] * unitPrices[i];
         SUMMARY.innerHTML += "<p>" + quantities[i] + "x " + Items1[i] + " - $" + lineTotal + " " + "<button onclick=\"decreaseQuantity('" + Items1[i] + "')\">-</button> " + "<button onclick=\"increaseQuantity('" + Items1[i] + "', " + unitPrices[i] + ")\">+</button>" + "</p>";
     }
-}
+
 
 function increaseQuantity(itemName, price) {
     itemList.push(itemName);
