d34441bcbbfba56dcddb01282d62fcb8bf1de1b9
David-24245 <24245dl@hvhs.school.nz>
2026-07-23 11:48:29 +0000

    Jul 23, 2026, 11:48 AM

--- receipt.js
+++ receipt.js
@@ -1,14 +1,19 @@
+
+/* This piece of code loads the customer's order, groups identical items together, and displays the completed receipt*/
 function displayReceipt(){
+    // Retrieves the saved customer information
     let userName = localStorage.getItem("userName");
     let userMoney = localStorage.getItem("userMoney");
     let orderTotal = localStorage.getItem("orderTotal");
 
+    // Retrieve the saved item and price lists
     let itemListRaw = localStorage.getItem("itemList");
     let priceListRaw = localStorage.getItem("priceList");
 
     let itemList;
     let priceList;
 
+    // If no items were saved, create empty arrays
     if (itemListRaw === "") {
         itemList = [];
         priceList = [];
