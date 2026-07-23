452f48aed93e4045bfaf8113a39f5a04035173ec
David-24245 <24245dl@hvhs.school.nz>
2026-07-23 22:54:06 +0000

    Jul 23, 2026, 10:54 PM

--- script.js
+++ script.js
@@ -2,15 +2,6 @@ console.log("Hello world!")
 
 //Variables
 let userName = "David";
-let year = 2026;
-let age = 15;
-let pocketMoney = 20;
-let birthYear;
-let oldAge;
-let halfMoney;
-birthYear = year - age;
-oldAge = age + 10;
-halfMoney = pocketMoney / 2;
 let itemList = [];
 let priceList = [];
 let orderTotal = 0;
